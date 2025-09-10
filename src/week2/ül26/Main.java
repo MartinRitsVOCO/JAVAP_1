40d4564fa4715ebd845bc63cd74716a5fac719d5
MartinRitsVOCO <martin.ritsberg@voco.ee>
2025-09-10 10:01:48 +0300

    Week 2 ülesanded jätk

---
+++ src/week2/ül26/Main.java
@@ -0,0 +1,23 @@
+package week2.ül26;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int sum = 0;
+        System.out.println("Enter a number to start adding numbers together, enter 0 to exit.");
+        while (true) {
+            int read = Integer.parseInt(reader.nextLine());
+            if (read == 0) {
+                break;
+            }
+
+            sum += read;
+
+            System.out.println("Sum now: " + sum);
+            System.out.println("Enter the next number to add.");
+        }
+
+        System.out.println("Sum in the end: " + sum);
+    }
+}
---
+++ src/week2/ül39/Main.java
@@ -0,0 +1,63 @@
+package week2.ül39;
+
+public class Main {
+    public static void main(String[] args) {
+        printStars(3);
+        printStars(7);
+        printStars(4);
+
+        System.out.println();
+
+        printSquare(6);
+
+        System.out.println();
+
+        printRectangle(5, 8);
+
+        System.out.println();
+
+        printTriangle(9);
+    }
+
+    private static void printStars(int amount) {
+        while (amount > 0) {
+            amount--;
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    private static void printSquare(int amount) {
+        printRectangle(amount, amount);
+
+/*  Alternatiivselt:
+        int width = amount;
+
+        while (amount > 0) {
+            amount--;
+            printStars(width);
+        }
+        System.out.println();
+*/
+    }
+
+    private static void printRectangle(int width, int height) {
+        while (height > 0) {
+            height--;
+            printStars(width);
+        }
+
+        System.out.println();
+    }
+
+    private static void printTriangle(int height) {
+        int width = 1;
+
+        while (height > 0) {
+            height--;
+            printStars(width);
+            width++;
+        }
+    }
+}
+
